4dd931cfd4b93346a02851474d0b3c2f1049b234
Bub Der <brudijacob@gmail.com>
2025-06-25 16:23:29 +0200

    fixes

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  // base: "/react_gsap_coctails/",
+  base: "/react_gsap_cocktails/",
 });
